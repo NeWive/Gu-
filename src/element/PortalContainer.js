e8c4c49c61fa23fe50507234de0f6b8c35d9b89a
newive <738767136@qq.com>
2019-09-06 20:44:30 +0800

    '败者食尘'

--- src/element/PortalContainer.js
+++ src/element/PortalContainer.js
@@ -1,10 +1,12 @@
 import { PureComponent } from 'react';
 import ReactDom from 'react-dom';
 const modalRoot = document.getElementById('modal-root');
+console.log(modalRoot);
 export class PortalContainer extends PureComponent {
     constructor(props){
         super(props);
         this.container = document.createElement('div');
+        console.log(this.container);
     }
     componentDidMount() {
         modalRoot.appendChild(this.container);
